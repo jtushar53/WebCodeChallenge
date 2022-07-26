5809b97b4cb47b385f148abc8579e654810e0a10
jtushar53 <jtushar53@gmail.com>
2022-07-26 21:52:06 +0530

    2 files modified

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   baseUrl: 'https://api.github.com/',
-  token: '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', // Not recommened to store this token here, github oauth flow is recommenede for front end apps
+  token: '', // Not recommened to store this token here, github oauth flow is recommenede for front end apps
 };
