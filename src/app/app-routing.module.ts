da514fd477a8dc636003217b37d2cb7eaafafc25
jtushar53 <jtushar53@gmail.com>
2022-07-26 14:08:05 +0530

    added routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'search',
+    pathMatch: 'full',
+  },
+  {
+    path: 'search',
+    loadChildren: () =>
+      import('../search/search.module').then((m) => m.SearchModule),
+  },
+  {
+    path: 'history',
+    loadChildren: () =>
+      import('../history/history.module').then((m) => m.HistoryModule),
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadAllModules,
+      useHash: true,
+    }),
+  ],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
